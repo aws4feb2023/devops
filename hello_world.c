36c1ac4e828c4d0319ea8dc47a67f526b9f6c127
Ubuntu <ubuntu@ip-172-31-36-163.ap-south-1.compute.internal>
2023-04-13 13:23:04 +0000

    first 2 files

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void hello() 
+
+//int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
+	   
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() 
+{
+ hello_world.o();
+addno.o();
+piryamid.o();
+fact.o();
+}
+
